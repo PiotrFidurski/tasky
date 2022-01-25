05de00a43bbcca1525630b76d8d219b76d28d2a8
Piotr Fidurski <crunor55@hotmail.com>
2022-01-25 15:21:07 +0100

    refactor: extract session utilty functions
    Create getUserSession and destroyUserSession utility functions

--- app/session/session.server.ts
+++ app/session/session.server.ts
@@ -1,4 +1,7 @@
-import { createCookieSessionStorage } from 'remix';
+import {
+  createCookieSessionStorage,
+  redirect,
+} from 'remix';
 
 const SECRET_KEY = process.env.SESSION_SECRET;
 
@@ -16,3 +19,42 @@ export const sessionStorage = createCookieSessionStorage({
     secure: process.env.NODE_ENV === 'production',
   },
 });
+
+/**
+ * Parses Cookie header and returns session promise.
+ *
+ * @param {Request} request - request Fetch API. https://developer.mozilla.org/en-US/docs/Web/API/Request.
+ * @returns `Promise<Session>`
+ */
+
+export function getUserSession({
+  request,
+}: {
+  request: Request;
+}) {
+  return sessionStorage.getSession(
+    request.headers.get('Cookie')
+  );
+}
+
+/**
+ * Destroys user session and redirects to login page.
+ *
+ * @param {Request} request - request Fetch API. https://developer.mozilla.org/en-US/docs/Web/API/Request.
+ * @returns `Promise<Response>` - https://developer.mozilla.org/en-US/docs/Web/API/Response
+ */
+export async function destroyUserSession({
+  request,
+}: {
+  request: Request;
+}) {
+  const session = await getUserSession({ request });
+
+  return redirect('/login', {
+    headers: {
+      'Set-Cookie': await sessionStorage.destroySession(
+        session
+      ),
+    },
+  });
+}
