403dd880d16249d31c0c94958f4ba06b76bd4e3d
Piotr Fidurski <crunor55@hotmail.com>
2022-01-26 12:24:32 +0100

    refactor: remove remix-auth
    Ended up using alot of our own login for authentication so we moved that
    logic to actions instead

--- app/session/auth.server.ts
+++ app/session/auth.server.ts
@@ -1,18 +1,9 @@
 import { User } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import { redirect } from 'remix';
-import { Authenticator } from 'remix-auth';
-import { FormStrategy } from 'remix-auth-form';
 import * as z from 'zod';
 import { db } from '~/db/db.server';
-import { badRequest } from '~/utils/badRequest';
-import {
-  createUserSession,
-  getUserSession,
-  sessionStorage,
-} from './session.server';
-
-export const authenticator = new Authenticator<User | Response>(sessionStorage);
+import { getUserSession } from './session.server';
 
 /**
  * Partial fields of User Model
@@ -45,12 +36,21 @@ export async function getUser({ request }: { request: Request }) {
 const PWD_REGEX_PATTERN =
   /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/;
 
-const loginSchema = z.object({
+export const loginSchema = z.object({
   username: z
     .string({
       required_error: 'Username is required.',
     })
     .min(3, 'Username must be at least 3 characters long.'),
+  password: z
+    .string({
+      required_error: 'Password is required.',
+    })
+    .regex(
+      PWD_REGEX_PATTERN,
+      'Password must include special characters, numbers, and upper case letters.'
+    )
+    .min(8, 'Password must be at least 8 characters long.'),
 });
 
 const passwordValidation = z.object({
@@ -68,7 +68,7 @@ const passwordValidation = z.object({
   }),
 });
 
-const registerSchema = loginSchema
+export const registerSchema = loginSchema
   .merge(passwordValidation)
   .refine((data) => data.password === data.passwordConfirmation, {
     message: "Provided passwords don't match.",
@@ -82,7 +82,7 @@ const registerSchema = loginSchema
  * @param {string} password - Password of the user.
  * @returns `Promise<User>`
  */
-async function register({ username, password }: UserInput) {
+export async function register({ username, password }: UserInput) {
   const passwordHash = await bcrypt.hash(password, 10);
 
   return db.user.create({
@@ -106,87 +106,3 @@ export async function login({ username }: UserInput) {
 
   return user;
 }
-
-authenticator.use(
-  new FormStrategy(async ({ form }) => {
-    try {
-      const type = form.get('type') as string;
-
-      const username = form.get('username') as string;
-      const password = form.get('password') as string;
-      const passwordConfirmation = form.get('passwordConfirmation') as string;
-
-      let user = null;
-
-      switch (type) {
-        case 'login':
-          {
-            loginSchema.parse({ username, password });
-
-            user = await login({ username, password });
-
-            if (!user) {
-              return badRequest({
-                fieldErrors: {
-                  username: `No user with that username exists.`,
-                },
-              });
-            }
-
-            const isPasswordCorrect = await bcrypt.compare(
-              password,
-              user.password
-            );
-
-            if (!isPasswordCorrect) {
-              return badRequest({
-                fieldErrors: {
-                  password: `The password you provided doesn't match.`,
-                },
-              });
-            }
-          }
-          break;
-        case 'register':
-          {
-            registerSchema.parse({ username, password, passwordConfirmation });
-
-            const existingUser = await db.user.findFirst({
-              where: { username },
-            });
-
-            if (existingUser) {
-              return badRequest({
-                fieldErrors: {
-                  username: `This username is already taken.`,
-                },
-              });
-            }
-
-            user = await register({ username, password });
-          }
-          break;
-        default:
-          break;
-      }
-
-      if (user) {
-        return await createUserSession({ user });
-      }
-
-      return badRequest({
-        formError: 'Something went wrong, try again in a moment.',
-      });
-    } catch (error) {
-      const errors = (error as z.ZodError).flatten();
-
-      return badRequest({
-        fieldErrors: {
-          username: errors.fieldErrors.username?.[0],
-          password: errors.fieldErrors.password?.[0],
-          passwordConfirmation: errors.fieldErrors.passwordConfirmation?.[0],
-        },
-      });
-    }
-  })
-);
