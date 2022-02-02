3c250b06b68d964887298aae6263c83171ad7769
Piotr Fidurski <crunor55@hotmail.com>
2022-02-02 21:59:59 +0100

    refactor: remove getUser utility

--- app/session/auth.server.ts
+++ app/session/auth.server.ts
@@ -1,8 +1,6 @@
 import { User } from '@prisma/client';
 import bcrypt from 'bcryptjs';
-import { redirect } from 'remix';
 import { db } from '~/db/db.server';
-import { getUserSession } from './session.server';
 
 /**
  * Partial fields of User Model
@@ -10,32 +8,12 @@ import { getUserSession } from './session.server';
 type UserInput = Pick<User, 'username' | 'password'>;
 
 /**
- * Retrieves user for the session from the database.
- *
- * @param {Request} request - request Fetch API. https://developer.mozilla.org/en-US/docs/Web/API/Request
- * @returns `User` | `Response` - https://developer.mozilla.org/en-US/docs/Web/API/Response
- */
-export async function getUser({ request }: { request: Request }) {
-  const session = await getUserSession({ request });
-
-  if (!session.has('userId')) {
-    return redirect('/login');
-  }
-
-  const user = await db.user.findFirst({
-    where: { id: session.data?.userId },
-  });
-
-  return user;
-}
-
-/**
  * Checks database for user existence.
  *
- * @param {string} username - Username of the user.
+ * @param username - Username of the user.
  * @returns `User` or `null`
  */
-export async function login({ username }: UserInput) {
+export async function getUser(username: string) {
   const user = await db.user.findFirst({
     where: { username },
   });
