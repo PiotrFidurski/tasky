0facb03d0f06073798e5b6117c2d716f9842768b
Piotr Fidurski <crunor55@hotmail.com>
2022-02-03 20:01:41 +0100

    fix(test): add minLength attr to password field

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -86,6 +86,7 @@ export default function LoginRoute() {
         <label htmlFor="password">
           <span className="text-slate-900 font-semibold">Password</span>
           <input
+            minLength={8}
             className="border-b-2 mt-2 border-slate-600 px-2 py-2 w-full focus:outline-none focus:border-b-blue-600 focus:bg-blue-200 transition-colors"
             required
             id="password"
