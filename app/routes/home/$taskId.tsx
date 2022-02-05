53656cc79c314d92441d2fe511e1dbe346bc0d02
Piotr Fidurski <crunor55@hotmail.com>
2022-02-05 15:29:38 +0100

    feat: get task data from params

--- app/routes/home/$taskId.tsx
+++ app/routes/home/$taskId.tsx
@@ -1,3 +1,30 @@
+import { Task } from '@prisma/client';
+import { LoaderFunction, useLoaderData } from 'remix';
+import { db } from '~/db/db.server';
+import { getErrorMessage } from '~/utils/getErrorMessage';
+
+type LoaderData = {
+  task: Task | null;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  try {
+    const { taskId } = params;
+
+    const task = await db.task.findFirst({ where: { id: taskId } });
+
+    const data: LoaderData = {
+      task,
+    };
+
+    return data;
+  } catch (error) {
+    return getErrorMessage(error);
+  }
+};
+
 export default function TaskRoute() {
-  return <div>task route</div>;
+  const { task } = useLoaderData<LoaderData>();
+
+  return <div>task route, {task?.body}</div>;
 }
