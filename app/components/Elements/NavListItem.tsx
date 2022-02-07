b9d394cd8660f947055f1ab6a2d02bde74e5cec0
Piotr Fidurski <crunor55@hotmail.com>
2022-02-07 14:25:35 +0100

    refactor: fix class repetition

--- app/components/Elements/NavListItem.tsx
+++ app/components/Elements/NavListItem.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+
 import { NavLink } from 'remix';
 
 type ListItemProps = React.LiHTMLAttributes<HTMLLIElement> & { to: string };
@@ -12,11 +14,10 @@ function NavListItem({ children, to, ...props }: ListItemProps) {
       <NavLink to={to}>
         {({ isActive }) => (
           <div
-            className={
-              isActive
-                ? 'flex items-center gap-2 px-2 py-2 border bg-blue-600 rounded-md text-white'
-                : 'flex items-center gap-2 px-2 py-2 border rounded-md text-slate-600 shadow-md'
-            }
+            className={clsx(
+              'flex items-center gap-2 px-2 py-2 border rounded-md shadow-sm',
+              isActive ? 'bg-blue-600 text-white' : 'text-slate-600'
+            )}
           >
             {children}
           </div>
