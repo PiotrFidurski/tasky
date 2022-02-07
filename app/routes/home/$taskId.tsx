ae45c7aa8ca16ea6d9828c9fac27fd35f78c01ea
Piotr Fidurski <crunor55@hotmail.com>
2022-02-07 18:18:41 +0100

    fix: use CatchBoundary in $taskId route

--- app/routes/home/$taskId.tsx
+++ app/routes/home/$taskId.tsx
@@ -1,42 +1,51 @@
 import { Task } from '@prisma/client';
 
-import { LoaderFunction, useLoaderData } from 'remix';
+import { LoaderFunction, useCatch, useLoaderData } from 'remix';
 
 import { getTask } from '~/models/task';
 
 import { badRequest } from '~/utils/badRequest';
-import { getErrorMessage } from '~/utils/getErrorMessage';
 
 type LoaderData = {
   task: Task;
 };
 
 export const loader: LoaderFunction = async ({ params }) => {
-  try {
-    const { taskId } = params;
+  const { taskId } = params;
 
-    if (!taskId) {
-      return null;
-    }
-
-    const task = await getTask(taskId);
-
-    if (!task) {
-      return badRequest("There used to be something here but now it's gone");
-    }
-
-    const data: LoaderData = {
-      task,
-    };
-
-    return data;
-  } catch (error) {
-    return getErrorMessage(error);
+  if (!taskId) {
+    return null;
   }
+
+  const task = await getTask(taskId);
+
+  if (!task) {
+    throw badRequest("There used to be something here but now it's gone");
+  }
+
+  const data: LoaderData = {
+    task,
+  };
+
+  return data;
 };
 
 export default function TaskRoute() {
   const { task } = useLoaderData<LoaderData>();
 
   return <div>task route, {task?.body}</div>;
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <div>
+      <h1>Caught</h1>
+      <p>Status: {caught.status}</p>
+      <pre>
+        <code>{JSON.stringify(caught.data, null, 2)}</code>
+      </pre>
+    </div>
+  );
 }
