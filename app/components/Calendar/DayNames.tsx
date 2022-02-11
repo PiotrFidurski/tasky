bd8190106ad0e13119a2d167b972a977439173a3
Piotr Fidurski <crunor55@hotmail.com>
2022-02-11 19:44:08 +0100

    refactor: update WeekDayNames style
    use calc function to calculate the width of container

--- app/components/Calendar/DayNames.tsx
+++ app/components/Calendar/DayNames.tsx
@@ -2,11 +2,11 @@ import { weekDayNames } from '~/utils/date';
 
 function WeekDayNames() {
   return (
-    <div className="flex sticky top-0">
+    <div className="flex sticky top-0 shadow-md">
       {weekDayNames.map((week) => (
         <span
           key={week}
-          className="w-[3rem] h-[3rem] flex items-center justify-center uppercase bg-blue-600 text-white font-bold shadow-sm"
+          className="w-[calc(340px/7)] h-[3rem] flex items-center justify-center uppercase bg-blue-600 text-white font-bold shadow-sm"
         >
           {week}
         </span>
