9188a1ff9204888893570e227e8cabd146f6f6a4
Piotr Fidurski <crunor55@hotmail.com>
2022-02-12 08:49:56 +0100

    refactor: remove months array

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,43 +1,43 @@
-import { addDays, format, getISOWeeksInYear, startOfWeek } from 'date-fns';
+import {
+  addDays,
+  format,
+  getISOWeeksInYear,
+  startOfWeek,
+  startOfYear,
+} from 'date-fns';
 
 export const weekDayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-export const months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec',
-];
 
 export function formatDate(date: Date = new Date()) {
   return format(date, 'yyyy-MM-dd');
 }
 
 const NUMBER_OF_WEEKS_FOR_YEAR = getISOWeeksInYear(new Date());
-const START_OF_WEEK = startOfWeek(new Date(), { weekStartsOn: 0 });
 
 const weekRows = new Array(NUMBER_OF_WEEKS_FOR_YEAR + 1).fill(null);
 
-export function createCalendar() {
-  const calendarMatrix: Array<Array<Date>> = [];
+type Props = {
+  weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | undefined;
+  date?: Date;
+};
 
-  let currentDay = START_OF_WEEK;
+export function getCalendarData({
+  weekStartsOn = 0,
+  date = startOfYear(new Date()),
+}: Props = {}) {
+  let startFromDay = startOfWeek(date, {
+    weekStartsOn,
+  });
+
+  const calendarMatrix: Array<Array<Date>> = [];
 
   weekRows.forEach(() => {
     const week: Array<Date> = [];
 
     weekDayNames.forEach(() => {
-      week.push(currentDay);
+      week.push(startFromDay);
 
-      currentDay = addDays(currentDay, 1);
+      startFromDay = addDays(startFromDay, 1);
     });
 
     calendarMatrix.push(week);
