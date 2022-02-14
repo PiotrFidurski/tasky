42753aadae9d6b4f1dca81b2fc280921292aaf01
Piotr Fidurski <crunor55@hotmail.com>
2022-02-14 09:33:37 +0100

    fix: fix a complete task bug
    whenever we would click complete button quicky it would cause task to
    disappear

--- app/components/Tasks/TaskComponent.tsx
+++ app/components/Tasks/TaskComponent.tsx
@@ -20,65 +20,57 @@ function TaskComponent({ task }: { task: Task }) {
     return task.isComplete;
   };
 
-  const isScheduling = () => {
-    const action = fetcher?.submission?.formData.get('_action');
-
-    if (action) {
-      return (
-        action === actionTypes.SCHEDULE_TASK || actionTypes.UNSCHEDULE_TASK
-      );
-    }
-
-    return false;
-  };
+  const isScheduling =
+    fetcher.submission?.formData.get('_action') === actionTypes.SCHEDULE_TASK;
+  const isUnscheduling =
+    fetcher.submission?.formData.get('_action') === actionTypes.UNSCHEDULE_TASK;
 
   return (
     <article
-      style={{ display: isScheduling() ? 'none' : 'flex' }}
+      style={{ display: isScheduling || isUnscheduling ? 'none' : 'flex' }}
       aria-label={task.body}
       className={`${
         isComplete() ? 'bg-blue-100' : ''
       } py-4 px-2 shadow-md mb-2 rounded flex items-center justify-between`}
     >
       <div className="flex items-center gap-2">
-        {day ? (
-          <fetcher.Form method="post">
-            <input
-              name="_action"
-              value={
-                isComplete()
-                  ? actionTypes.MARK_TASK_UNCOMPLETE
-                  : actionTypes.MARK_TASK_COMPLETE
-              }
-              type="hidden"
-            />
-            <input name="id" value={task.id} type="hidden" />
-            <button
-              type="submit"
-              aria-label={isComplete() ? 'uncomplete task' : 'complete task'}
-              className="rounded-full border-2 border-slate-600 text-blue-600 align-bottom"
-            >
-              {/* checkmark icon */}
-              {isComplete() ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              ) : (
-                <div className="w-5 h-5" />
-              )}
-            </button>
-          </fetcher.Form>
-        ) : null}
-        {!task.scheduledFor && day ? (
+        <fetcher.Form method="post">
+          <input
+            name="_action"
+            value={
+              isComplete()
+                ? actionTypes.MARK_TASK_UNCOMPLETE
+                : actionTypes.MARK_TASK_COMPLETE
+            }
+            type="hidden"
+          />
+          <input name="id" value={task.id} type="hidden" />
+          <button
+            type="submit"
+            aria-label={isComplete() ? 'uncomplete task' : 'complete task'}
+            className="rounded-full border-2 border-slate-600 text-blue-600 align-bottom"
+          >
+            {/* checkmark icon */}
+            {isComplete() ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            ) : (
+              <div className="w-5 h-5" />
+            )}
+          </button>
+        </fetcher.Form>
+
+        {!task.scheduledFor ? (
           <fetcher.Form method="post">
             <input
               name="_action"
