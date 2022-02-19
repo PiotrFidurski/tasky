0d065f33946fdf1745cd5f8e30add33adb09c42c
Piotr Fidurski <crunor55@hotmail.com>
2022-02-19 11:46:57 +0100

    refactor: create dropdown item custom component

---
+++ app/components/Elements/DropdownItem.tsx
@@ -0,0 +1,27 @@
+import { DropdownMenuItemProps, Item } from '@radix-ui/react-dropdown-menu';
+import clsx from 'clsx';
+import { forwardRef } from 'react';
+
+type DropdownItemProps = DropdownMenuItemProps &
+  React.RefAttributes<HTMLDivElement>;
+
+export const DropdownItem = forwardRef<HTMLDivElement, DropdownItemProps>(
+  // eslint-disable-next-line react/prop-types
+  ({ className, children, ...props }, ref) => {
+    return (
+      <Item
+        ref={ref}
+        className={clsx(
+          `min-h-[2rem] text-custom__gray font-semibold flex items-center gap-4
+           px-2 py-4 outline-none focus:bg-custom__hoverlight fill-custom__gray
+         dark:text-custom__ghostly focus:dark:bg-custom__hoverdark dark:fill-custom__ghostly`,
+          className
+        )}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+      >
+        {children}
+      </Item>
+    );
+  }
+);
