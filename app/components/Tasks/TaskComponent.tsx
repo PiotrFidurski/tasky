b789369430c7d381a075695c91c92e5865409882
Piotr Fidurski <crunor55@hotmail.com>
2022-02-25 13:31:02 +0100

    feat: add update task button

--- app/components/Tasks/TaskComponent.tsx
+++ app/components/Tasks/TaskComponent.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import { motion } from 'framer-motion';
 import { actionTypes } from '~/actions/actionTypes';
 
-import { useFetcher, useLocation, useMatches, useParams } from 'remix';
+import { Link, useFetcher, useLocation, useMatches, useParams } from 'remix';
 
 import { formatDate } from '~/utils/date';
 
@@ -174,7 +174,7 @@ function TaskComponent({ task }: { task: Task }) {
         </p>
       </div>
       {currentUserId === task.userId ? (
-        <div className="p-1 flex items-center">
+        <div className="p-1 flex gap-4 items-center">
           <fetcher.Form method="post">
             <input
               name="_action"
@@ -205,6 +205,29 @@ function TaskComponent({ task }: { task: Task }) {
               </svg>
             </Button>
           </fetcher.Form>
+          <Link to={`${task.id}/edit`}>
+            <Button
+              type="submit"
+              isGhost
+              className="text-green-600 border-green-600 rounded-full p-1 hover:bg-green-600 hover:border-green-600"
+              aria-label="update task"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                />
+              </svg>
+            </Button>
+          </Link>
         </div>
       ) : null}
     </motion.article>
