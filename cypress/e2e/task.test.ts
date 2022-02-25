a4ade85bfe87682977aa0d1455dae1c03f36c396
Piotr Fidurski <crunor55@hotmail.com>
2022-02-25 20:29:43 +0100

    test: update task tests

--- cypress/e2e/task.test.ts
+++ cypress/e2e/task.test.ts
@@ -10,26 +10,32 @@ describe('task', () => {
   it('should allow users to create new tasks', () => {
     cy.visit('/home');
 
-    cy.findByRole('textbox', { name: /body/i }).type(
-      `this is a test task.{enter}`
-    );
-    // TODO: update later when we introduce easier task creating on calendar page
+    cy.findByRole('link', { name: /create task/i }).click();
+
+    cy.findByRole('textbox', { name: /body/i }).type('this is a test task');
+
+    cy.findByRole('button', { name: /add task/i }).click();
+
+    cy.findByText(/this is a test task/i);
   });
 
   it('should show field errors if there are any.', () => {
     cy.visit('/home');
 
-    cy.findByRole('textbox', { name: /body/i })
-      .as('bodyInput')
-      .type(`k.{enter}`);
+    cy.findByRole('link', { name: /create task/i }).click();
+
+    cy.findByRole('textbox', { name: /body/i }).type('k');
+    cy.findByRole('button', { name: /add task/i }).click();
 
     cy.findByText(/Body should be at least 3 characters long./i);
   });
 
   it('should allow users to complete tasks.', () => {
-    cy.visit('/calendar');
+    cy.visit('/home');
 
-    cy.findByRole('article', { name: /this is a test task./i }).within(() => {
+    cy.findByRole('link', { name: /calendar/i }).click();
+
+    cy.findByRole('article', { name: /this is a test task/i }).within(() => {
       cy.findByRole('button', { name: /complete task/i }).as('completeTaskBtn');
 
       cy.get('@completeTaskBtn').click();
@@ -49,14 +55,14 @@ describe('task', () => {
 
     cy.findByRole('link', { name: date }).click();
 
-    cy.findByRole('article', { name: /this is a test task./i }).within(() => {
+    cy.findByRole('article', { name: /this is a test task/i }).within(() => {
       cy.findByRole('button', { name: /schedule task/i }).click();
     });
 
     cy.findByRole('region', { name: date }).within(() => {
-      cy.findByText(/this is a test task./);
+      cy.findByText(/this is a test task/);
 
-      cy.findByRole('article', { name: /this is a test task./i }).within(() => {
+      cy.findByRole('article', { name: /this is a test task/i }).within(() => {
         cy.findByRole('button', { name: /unschedule task/i }).click();
       });
     });
@@ -65,9 +71,9 @@ describe('task', () => {
   it('should allow users to delete a task.', () => {
     cy.visit('/home');
 
-    cy.findByRole('textbox', { name: /body/i }).type(`delete me later{enter}`);
+    cy.findByRole('link', { name: /create task/i }).click();
 
-    cy.findByRole('link', { name: /calendar/i }).click();
+    cy.findByRole('textbox', { name: /body/i }).type(`delete me later{enter}`);
 
     cy.findByRole('article', { name: /delete me later/i }).within(() => {
       cy.findByRole('button', { name: /delete task/i }).click();
