29052e97816b5aecb62ff36b7e33ac89bc64d033
Piotr Fidurski <crunor55@hotmail.com>
2022-02-25 13:30:39 +0100

    refactor: wrap Button in forwardRef

--- app/components/Elements/Button.tsx
+++ app/components/Elements/Button.tsx
@@ -1,33 +1,32 @@
 import clsx from 'clsx';
+import { forwardRef } from 'react';
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   isGhost?: boolean;
   buttonType?: boolean;
 };
 
-function Button({
-  children,
-  isGhost,
-  buttonType,
-  className,
-  ...htmlButtonProps
-}: ButtonProps) {
-  return (
-    <button
-      className={clsx(
-        'border-2 border-blue-600 w-full uppercase font-bold focus:outline-dashed outline-offset-2 focus:outline-2 focus:outline-blue-900 transition-colors',
-        isGhost
-          ? 'bg-transparent text-blue-600 hover:text-white hover:bg-blue-600 hover:border-blue-600'
-          : 'bg-blue-600 text-white hover:bg-blue-700 hover:border-blue-700',
-        className
-      )}
-      type={buttonType ? 'button' : 'submit'}
-      {...htmlButtonProps}
-    >
-      {children}
-    </button>
-  );
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  // eslint-disable-next-line react/prop-types
+  ({ children, isGhost, buttonType, className, ...htmlButtonProps }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={clsx(
+          'border-2 border-blue-600 w-full uppercase font-bold focus:outline-dashed outline-offset-2 focus:outline-2 focus:outline-blue-900 transition-colors',
+          isGhost
+            ? 'bg-transparent text-blue-600 hover:text-white hover:bg-blue-600 hover:border-blue-600'
+            : 'bg-blue-600 text-white hover:bg-blue-700 hover:border-blue-700',
+          className
+        )}
+        type={buttonType ? 'button' : 'submit'}
+        {...htmlButtonProps}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 Button.defaultProps = {
   isGhost: false,
