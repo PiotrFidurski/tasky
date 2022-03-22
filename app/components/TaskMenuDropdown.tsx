a85a12e9f263779ed8b5b6177fae6ed292b9e3de
PiotrFidurski <crunor55@hotmail.com>
2022-03-22 18:21:10 +0100

    refactor: add style to dropdown actions

--- app/components/TaskMenuDropdown.tsx
+++ app/components/TaskMenuDropdown.tsx
@@ -5,7 +5,7 @@ import { actionTypes } from '~/actions/actionTypes';
 import { Link, useFetcher } from 'remix';
 
 import { useUser } from './Auth/useUser';
-import { Button } from './Elements/Button';
+import { DropdownItem } from './Elements/DropdownItem';
 
 type TaskMenuDropdownProps = {
   task: Task;
@@ -49,21 +49,16 @@ export function TaskMenuDropdown({ task }: TaskMenuDropdownProps) {
           offset={20}
         />
         {user?.id === task.userId ? (
-          <div className="p-1 flex gap-4 items-center">
-            <fetcher.Form method="post">
-              <input
-                name="_action"
-                value={actionTypes.DELETE_TASK}
-                type="hidden"
-              />
-              <input name="id" value={task.id} type="hidden" />
-              <input name="ownerId" value={task.userId} type="hidden" />
-              <Button
-                type="submit"
-                isGhost
-                className="text-rose-600 border-rose-600 rounded-full p-1 hover:bg-rose-600 hover:border-rose-600"
-                aria-label="delete task"
-              >
+          <fetcher.Form method="post">
+            <button type="submit" aria-label="delete task" className="w-full">
+              <DropdownItem onSelect={(e) => e.preventDefault()}>
+                <input
+                  name="_action"
+                  value={actionTypes.DELETE_TASK}
+                  type="hidden"
+                />
+                <input name="id" value={task.id} type="hidden" />
+                <input name="ownerId" value={task.userId} type="hidden" />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6"
@@ -78,32 +73,31 @@ export function TaskMenuDropdown({ task }: TaskMenuDropdownProps) {
                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                   />
                 </svg>
-              </Button>
-            </fetcher.Form>
-            <Link to={`${task.id}/edit`}>
-              <Button
-                type="submit"
-                isGhost
-                className="text-green-600 border-green-600 rounded-full p-1 hover:bg-green-600 hover:border-green-600"
-                aria-label="update task"
+                <span>Delete Task</span>
+              </DropdownItem>
+            </button>
+          </fetcher.Form>
+        ) : null}
+        {user?.id === task.userId ? (
+          <DropdownItem>
+            <Link to={`${task.id}/edit`} className="flex items-center gap-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                  />
-                </svg>
-              </Button>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                />
+              </svg>
+              <span>Edit Task</span>
             </Link>
-          </div>
+          </DropdownItem>
         ) : null}
       </DropdownMenu.Content>
     </DropdownMenu.Root>
