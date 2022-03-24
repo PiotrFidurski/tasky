b3d6b7124ccb53f59fba7db0bb1d664ebc71dce6
PiotrFidurski <crunor55@hotmail.com>
2022-03-24 18:44:55 +0100

    refactor: extract UserAvatar component

---
+++ app/components/UserAvatar.tsx
@@ -0,0 +1,11 @@
+export function UserAvatar() {
+  return (
+    <img
+      src="//unsplash.it/40/40"
+      width="50px"
+      height="50px"
+      alt="user avatar"
+      className="rounded-full"
+    />
+  );
+}
