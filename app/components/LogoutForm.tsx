7261f05fcfe19fcb18f888ab66bc4b6893f96302
PiotrFidurski <crunor55@hotmail.com>
2022-03-24 18:44:16 +0100

    refactor: extract LogoutForm component

---
+++ app/components/LogoutForm.tsx
@@ -0,0 +1,17 @@
+import { Form } from 'remix';
+
+import { LogoutIcon } from './Icons/LogoutIcon';
+
+export function LogoutForm() {
+  return (
+    <Form action="/logout" method="post" className="w-full">
+      <button
+        type="submit"
+        className="w-full flex items-center gap-4 px-2 py-4 font-semibold"
+      >
+        <LogoutIcon />
+        <span>Logout</span>
+      </button>
+    </Form>
+  );
+}
