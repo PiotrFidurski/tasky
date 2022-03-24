157d8bbc5714896f00319f7df0b62c0aebc46a4c
PiotrFidurski <crunor55@hotmail.com>
2022-03-24 18:43:46 +0100

    refactor: simplify SettingsMenu component

--- app/components/SettingsMenu.tsx
+++ app/components/SettingsMenu.tsx
@@ -1,11 +1,14 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
-import { Form } from 'remix';
-
 import { useUser } from './Auth/useUser';
 import { DropdownItem } from './Elements/DropdownItem';
+import { ProfileIcon } from './Icons/ProfileIcon';
+import { SettingsIcon } from './Icons/SettingsIcon';
+import { LogoutForm } from './LogoutForm';
 import { useTheme } from './Theme/ThemeProvider';
 import { Theme } from './Theme/themeContext';
+import { ToggleThemeButton } from './ToggleThemeButton';
+import { UserAvatar } from './UserAvatar';
 
 export function UserMenuDropdown() {
   const { theme, switchTheme } = useTheme();
@@ -20,13 +23,7 @@ export function UserMenuDropdown() {
           type="button"
           className="p-1 rounded-full hover:bg-custom__hoverlight dark:hover:bg-custom__hoverdark active:outline-custom__hoverlight focus:outline-2 focus:outline-offset-4 focus:outline-custom__gray focus:dark:outline-custom__ghostly"
         >
-          <img
-            src="//unsplash.it/40/40"
-            width="50px"
-            height="50px"
-            alt="user avatar"
-            className="rounded-full"
-          />
+          <UserAvatar />
         </button>
       </DropdownMenu.Trigger>
       <DropdownMenu.Content
@@ -41,69 +38,15 @@ export function UserMenuDropdown() {
           aria-label="user profile"
           className="rounded-tl-md rounded-tr-md px-2 py-4"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-            />
-          </svg>
-          <span>Profile, {user?.username}</span>
+          <ProfileIcon />
+          <span>{user?.username}</span>
         </DropdownItem>
         <DropdownItem aria-label="settings" className="px-2 py-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
+          <SettingsIcon />
           <span>Settings</span>
         </DropdownItem>
         <DropdownItem aria-label="logout" onSelect={(e) => e.preventDefault()}>
-          <Form action="/logout" method="post" className="w-full">
-            <button
-              type="submit"
-              className="w-full flex items-center gap-4 px-2 py-4 font-semibold"
-            >
-              {/* logout icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                />
-              </svg>
-              <span>Logout</span>
-            </button>
-          </Form>
+          <LogoutForm />
         </DropdownItem>
         <DropdownMenu.Label className="text-gray-600 font-semibold text-xs py-4 px-2 dark:text-gray-400">
           <span>Options</span>
@@ -119,45 +62,7 @@ export function UserMenuDropdown() {
           }
           onSelect={(e) => e.preventDefault()}
         >
-          <button
-            type="button"
-            className="flex w-full px-2 py-4 gap-4 font-semibold"
-          >
-            {/* toggle theme icon */}
-            {theme === Theme.light ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              </svg>
-            )}
-
-            <span>{theme === Theme.light ? 'Dark' : 'Light'}</span>
-          </button>
+          <ToggleThemeButton />
         </DropdownItem>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
