2972dd4f3ac03c538ea1f3d760e9dd043f7aa272
PiotrFidurski <crunor55@hotmail.com>
2022-04-06 19:10:02 +0200

    refactor: remove comments

--- app/components/Menus/MobileNav.tsx
+++ app/components/Menus/MobileNav.tsx
@@ -9,14 +9,13 @@ import { formatDate } from '~/utils/date';
 import { CustomLink } from '../Elements/CustomLink';
 import { PlusIcon } from '../Icons/PlusIcon';
 
-// style="position: absolute;/* inset: 0; *//* left: 50%; */right: calc(50% - 32px);top: -31px;height: 65px;width: 65px;background: #020304;border-radius: 100%;box-shadow: 0px 0px 7px 2px #8d8ab7f2;
-
 export function MobileNav() {
   const { day } = useParams<'day'>();
 
   const dayParam = !day ? formatDate() : day;
   return (
     <nav className="bg-indigo-800 rounded-tl-[4rem] rounded-tr-[4rem]">
+      {/* clip path this */}
       <div className="z-[100] absolute right-[calc(50%-2rem)] top-[-2rem] h-[4rem] w-[4rem] bg-black rounded-full shadow-md shadow-indigo-100">
         <CustomLink
           to={`/calendar/${dayParam}/create`}
