273aa8cd97b49ea7de920beb8ee03514ea4db8ea
PiotrFidurski <crunor55@hotmail.com>
2022-04-07 19:53:00 +0200

    refactor: adjust spacing for nav items

--- app/components/Menus/MobileNav.tsx
+++ app/components/Menus/MobileNav.tsx
@@ -13,31 +13,36 @@ export function MobileNav() {
   const { day } = useParams<'day'>();
 
   const dayParam = !day ? formatDate() : day;
+
   return (
-    <nav className="bg-indigo-800 rounded-full">
+    <nav className="bg-slate-900">
       {/* clip path this */}
-      <div className="z-[100] absolute right-[calc(50%-2rem)] top-[-2rem] h-[4rem] w-[4rem] bg-black rounded-full">
+      <div className="z-[100] absolute right-[calc(50%-2rem)] top-[-1rem] h-[4rem] w-[4rem] rounded-full">
         <CustomLink
           to={`/calendar/${dayParam}/create`}
-          className="flex items-center justify-center absolute inset-0 p-4 rounded-full border-0 bg-indigo-300"
+          className="flex items-center justify-center absolute inset-0 p-4 rounded-full border border-slate-800 bg-slate-900"
         >
           <PlusIcon />
         </CustomLink>
       </div>
-      <ul className="flex justify-between items-center px-4 py-2">
-        <NavListItem to="/home" className="flex justify-center">
-          {({ isActive }) => <HomeIcon isFilled={isActive} />}
-        </NavListItem>
-        <NavListItem to="/calendar" className="flex justify-center">
-          {({ isActive }) => <CalendarIcon isFilled={isActive} />}
-        </NavListItem>
-        <NavListItem to="/home" className="flex justify-center">
-          {({ isActive }) => <HomeIcon isFilled={isActive} />}
-        </NavListItem>
-        <NavListItem to="/calendar" className="flex justify-center">
-          {({ isActive }) => <CalendarIcon isFilled={isActive} />}
-        </NavListItem>
-      </ul>
+      <div className="flex w-full justify-between">
+        <ul className="flex justify-between items-center px-4 py-1 max-w-[12rem] w-full">
+          <NavListItem to="/home" className="flex justify-center">
+            {({ isActive }) => <HomeIcon isFilled={isActive} />}
+          </NavListItem>
+          <NavListItem to="/calendar" className="flex justify-center">
+            {({ isActive }) => <CalendarIcon isFilled={isActive} />}
+          </NavListItem>
+        </ul>
+        <ul className="flex items-center justify-between px-4 py-1 max-w-[12rem] w-full">
+          <NavListItem to="/home" className="flex justify-center">
+            {({ isActive }) => <HomeIcon isFilled={isActive} />}
+          </NavListItem>
+          <NavListItem to="/calendar" className="flex justify-center">
+            {({ isActive }) => <CalendarIcon isFilled={isActive} />}
+          </NavListItem>
+        </ul>
+      </div>
     </nav>
   );
 }
