aa2a120c625f2093ae583602787849fe18d9a448
PiotrFidurski <crunor55@hotmail.com>
2022-04-12 20:33:11 +0200

    refactor: add active style to links

--- app/components/Elements/NavListItem.tsx
+++ app/components/Elements/NavListItem.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+
 import { NavLink } from 'remix';
 
 type ListItemProps = React.LiHTMLAttributes<HTMLLIElement> & {
@@ -10,7 +12,12 @@ export function NavListItem({ children, to, ...htmlLiProps }: ListItemProps) {
     <li {...htmlLiProps}>
       <NavLink
         to={to}
-        className="flex w-full items-center text-custom__gray dark:text-custom__ghostly hover:bg-[#1b2539] dark:hover:text-white p-1 rounded-md"
+        className={({ isActive }) =>
+          clsx(
+            isActive ? 'bg-[#1b2539]' : '',
+            'flex w-full items-center text-custom__gray dark:text-custom__ghostly hover:bg-[#1b2539] dark:hover:text-white p-1 rounded-md'
+          )
+        }
       >
         {({ isActive }) => (
           <div className="flex items-center gap-2">
