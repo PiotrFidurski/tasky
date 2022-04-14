fdf674728633f174a8d662827260ecf9d7059882
PiotrFidurski <crunor55@hotmail.com>
2022-04-14 19:43:51 +0200

    feat: add create task button to menu

--- app/components/Menus/WIPv2_Sidebar.tsx
+++ app/components/Menus/WIPv2_Sidebar.tsx
@@ -8,10 +8,11 @@ import { CloseIcon } from '../Icons/CloseIcon';
 import { EditIcon } from '../Icons/EditIcon';
 import { HamburgerIcon } from '../Icons/HamburgerIcon';
 import { HomeIcon } from '../Icons/HomeIcon';
+import { PlusIcon } from '../Icons/PlusIcon';
 import { UserAvatar } from '../SettingsMenu/UserAvatar';
 
 export function WIPv2Sidebar() {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
 
   const toggleMenu = () => {
     setShow(!show);
@@ -33,61 +34,67 @@ export function WIPv2Sidebar() {
         </Button>
         <span>Home</span>
       </div>
-      <nav
-        aria-label="sidebar"
-        id="sidebar"
+      <div
         className={clsx(
-          'absolute top-0 left-0 bottom-0 max-w-[60%] z-50 bg-gray-900 w-full h-full border-r border-r-gray-700 transition-all',
+          'flex flex-col justify-between absolute top-0 left-0 bottom-0 max-w-[60%] z-50 bg-gray-900 w-full h-full py-4 border-r border-r-gray-700 transition-all',
           show ? 'translate-x-0 visible' : 'translate-x-[-100%] invisible'
         )}
       >
-        <div className="flex items-center p-4">
-          <h2 className="w-full">Menu</h2>
-          <div className="flex justify-end">
-            <Button
-              isIconWrapper
-              onClick={toggleMenu}
-              buttonType
-              aria-controls="sidebar"
-              aria-label="close menu"
-              aria-expanded={show}
-            >
-              <CloseIcon />
-            </Button>
+        <nav aria-label="sidebar" id="sidebar">
+          <div className="flex items-center p-4">
+            <h2 className="w-full">Menu</h2>
+            <div className="flex justify-end">
+              <Button
+                isIconWrapper
+                onClick={toggleMenu}
+                buttonType
+                aria-controls="sidebar"
+                aria-label="close menu"
+                aria-expanded={show}
+              >
+                <CloseIcon />
+              </Button>
+            </div>
           </div>
+          <div className="p-4 flex mb-4">
+            <UserAvatar width={40} height={40} />
+          </div>
+          <div className="">
+            <ul className="flex flex-col">
+              <NavListItem to="/home">
+                {({ isActive }) => (
+                  <>
+                    <HomeIcon isFilled={isActive} />
+                    <span>Home</span>
+                  </>
+                )}
+              </NavListItem>
+              <NavListItem to="/calendar">
+                {({ isActive }) => (
+                  <>
+                    <CalendarIcon isFilled={isActive} />
+                    <span>Calendar</span>
+                  </>
+                )}
+              </NavListItem>
+              <NavListItem to="/calendar">
+                {({ isActive }) => (
+                  <>
+                    <EditIcon isFilled={isActive} />
+                    <span>Tasks</span>
+                  </>
+                )}
+              </NavListItem>
+            </ul>
+          </div>
+        </nav>
+        <div className="p-4">
+          <Button primary className="flex justify-between p-2">
+            <span>Create Task</span>
+            <PlusIcon />
+          </Button>
         </div>
-        <div className="p-4 flex mb-4">
-          <UserAvatar width={40} height={40} />
-        </div>
-        <div className="">
-          <ul className="flex flex-col">
-            <NavListItem to="/home">
-              {({ isActive }) => (
-                <>
-                  <HomeIcon isFilled={isActive} />
-                  <span>Home</span>
-                </>
-              )}
-            </NavListItem>
-            <NavListItem to="/calendar">
-              {({ isActive }) => (
-                <>
-                  <CalendarIcon isFilled={isActive} />
-                  <span>Calendar</span>
-                </>
-              )}
-            </NavListItem>
-            <NavListItem to="/calendar">
-              {({ isActive }) => (
-                <>
-                  <EditIcon isFilled={isActive} />
-                  <span>Tasks</span>
-                </>
-              )}
-            </NavListItem>
-          </ul>
-        </div>
-      </nav>
+      </div>
     </div>
   );
 }
