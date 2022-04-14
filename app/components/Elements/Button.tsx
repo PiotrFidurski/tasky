17e065f8dc632c2160dd2dd76926b8b8bfa94215
PiotrFidurski <crunor55@hotmail.com>
2022-04-14 19:34:06 +0200

    refactor: update menuItem button type styles

--- app/components/Elements/Button.tsx
+++ app/components/Elements/Button.tsx
@@ -5,6 +5,7 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   primary?: boolean;
   isIconWrapper?: boolean;
   buttonType?: boolean;
+  isMenuItem?: boolean;
 };
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
@@ -13,6 +14,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       children,
       primary,
       isIconWrapper,
+      isMenuItem,
       buttonType,
       className,
       ...htmlButtonProps
@@ -26,6 +28,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           'flex gap-4 items-center rounded-full dark:text-custom__ghostly w-full transition-all',
           primary &&
             'border bg-indigo-600 border-indigo-500 hover:bg-indigo-700 hover:border-indigo-600 text-custom__ghostly',
+          isMenuItem && 'px-2 py-4',
           isIconWrapper &&
             'p-2 border-none bg-transparent hover:bg-[#1b2539] focus:ring-pink-200 focus:ring-2 focus:ring-inset focus:outline-none focus:bg-[#1b2539]',
           className
