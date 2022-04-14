ad7aa190c8f3533eba22fb12c4f70c48c071ff7a
PiotrFidurski <crunor55@hotmail.com>
2022-04-14 19:28:10 +0200

    refactor: update menu item button style

--- app/components/Elements/Button.tsx
+++ app/components/Elements/Button.tsx
@@ -27,7 +27,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           primary &&
             'border bg-indigo-600 border-indigo-500 hover:bg-indigo-700 hover:border-indigo-600 text-custom__ghostly',
           isMenuItem &&
-            'font-bold px-2 py-4 border-none bg-transparent hover:bg-transparent',
+            'font-bold px-2 py-2 border-none bg-transparent hover:bg-transparent focus:ring-pink-200 focus:ring-2 focus:ring-inset focus:outline-none focus:bg-[#1b2539]',
           className
         )}
         type={buttonType ? 'button' : 'submit'}
