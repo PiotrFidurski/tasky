1469b9c7ac4ae17adc9aeaa7ba89d87680683afd
PiotrFidurski <crunor55@hotmail.com>
2022-04-19 20:24:03 +0200

    refactor: update sidebar max-width

--- app/components/Sidebar/index.desktop.tsx
+++ app/components/Sidebar/index.desktop.tsx
@@ -2,7 +2,7 @@ import { Menu } from './Menu';
 
 export function DesktopSidebar() {
   return (
-    <div className="relative w-full h-full hidden md:block">
+    <div className="w-full h-full hidden md:block max-w-[16rem]">
       <Menu show onHandleClose={() => {}} isMobile={false} />
     </div>
   );
