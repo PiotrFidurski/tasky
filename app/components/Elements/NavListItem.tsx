4e2121b97af86276d957556cfe999b3fe97898ef
PiotrFidurski <crunor55@hotmail.com>
2022-04-19 20:34:54 +0200

    refactor: format NavListItem classNames

--- app/components/Elements/NavListItem.tsx
+++ app/components/Elements/NavListItem.tsx
@@ -15,8 +15,9 @@ export function NavListItem({ children, to, ...htmlLiProps }: ListItemProps) {
         className={({ isActive }) =>
           clsx(
             isActive ? 'bg-[#1b2539]' : '',
-            `p-3 flex w-full items-center text-custom__gray dark:text-custom__ghostly hover:bg-[#1b2539] dark:hover:text-white
-            focus:ring-pink-200 focus:ring-2 focus:ring-inset focus:outline-none focus:bg-[#1b2539]
+            `p-3 flex w-full items-center text-custom__gray dark:text-custom__ghostly
+             hover:bg-[#1b2539] dark:hover:text-white focus:ring-pink-200 focus:ring-2 
+             focus:ring-inset focus:outline-none focus:bg-[#1b2539]
             `
           )
         }
