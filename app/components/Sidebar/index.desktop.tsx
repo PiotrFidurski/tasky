752532f66c47be347da1de70cefe049216759a09
PiotrFidurski <crunor55@hotmail.com>
2022-04-21 19:50:31 +0200

    feat: make sidebar fixed

--- app/components/Sidebar/index.desktop.tsx
+++ app/components/Sidebar/index.desktop.tsx
@@ -2,7 +2,7 @@ import { Menu } from './Menu';
 
 export function DesktopSidebar() {
   return (
-    <div className="w-full h-full hidden md:block max-w-[16rem]">
+    <div className="w-full h-full hidden md:block max-w-[16rem] fixed top-0 left-0">
       <Menu show onHandleClose={() => {}} isMobile={false} />
     </div>
   );
