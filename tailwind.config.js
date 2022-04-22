a024fd473a477a4d3dda857c33ec612a8c5d1981
PiotrFidurski <crunor55@hotmail.com>
2022-04-22 19:59:43 +0200

    config: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // new dark mode
+        highlight: '#00C6CF',
+        dark: '#050507',
+        active_dark: '#212121',
+        // new light mode
+        light: '#F3F3F4',
+
+        active: '#e7e7e7',
+        // old
         custom__bluedark: '#020304',
         custom__gray: '#0E1F2B',
         custom__hoverdark: 'rgba(255 255 255 / 10%)',
