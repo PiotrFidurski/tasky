1b9981103f6b3ad9ea7609884ee251d68f70d688
PiotrFidurski <crunor55@hotmail.com>
2022-04-22 20:36:41 +0200

    refactor: format classNames

--- app/components/Sidebar/index.mobile.tsx
+++ app/components/Sidebar/index.mobile.tsx
@@ -29,8 +29,9 @@ export function MobileSidebar() {
       ) : null}
       <div
         className={clsx(
-          `font-semibold text-lg flex flex-col justify-between fixed top-0 left-0 bottom-0 max-w-[60%]
-           z-50 bg-light dark:bg-dark w-full h-full py-4 transition-all`,
+          `font-semibold text-lg flex flex-col justify-between fixed top-0 
+          left-0 bottom-0 max-w-[60%] z-50 bg-light dark:bg-dark w-full
+          h-full py-4 transition-all`,
           expanded ? 'translate-x-0 visible' : 'translate-x-[-100%] invisible'
         )}
       >
