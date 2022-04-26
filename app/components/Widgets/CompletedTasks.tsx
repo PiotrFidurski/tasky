c3822be916cda49f2df53562a072ac5e9fde06f8
PiotrFidurski <crunor55@hotmail.com>
2022-04-26 17:40:08 +0200

    feat: add simple completed tasks widget ui

---
+++ app/components/Widgets/CompletedTasks.tsx
@@ -0,0 +1,22 @@
+export function CompletedTasks() {
+  return (
+    <div className="w-full border-2 border-highlight rounded-md p-8 bg-slate-900">
+      <div className="flex items-center justify-between">
+        <div className="w-full">
+          <p className="font-extrabold text-xl mb-2">
+            Great, your progress is almost done!
+          </p>
+          <span className="font-semibold text-sm">4 of 7 completed</span>
+        </div>
+        <div className="w-full flex justify-end">
+          <div className="rounded-full w-36 h-36 border-8 border-light relative">
+            <div className="rounded-full absolute inset-[-0.5rem] border-8 z-10 border-light border-t-highlight border-l-highlight" />
+            <span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-extrabold text-highlight text-4xl">
+              58%
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
