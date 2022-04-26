5f0c598e21de05dc3036768ab3e1881979a3aec4
PiotrFidurski <crunor55@hotmail.com>
2022-04-26 18:11:24 +0200

    refactor: use conic gradient for circular style

--- app/components/Widgets/CompletedTasks.tsx
+++ app/components/Widgets/CompletedTasks.tsx
@@ -9,8 +9,9 @@ export function CompletedTasks() {
           <span className="font-semibold text-sm">4 of 7 completed</span>
         </div>
         <div className="w-full flex justify-end">
-          <div className="rounded-full w-36 h-36 border-8 border-light relative">
-            <div className="rounded-full absolute inset-[-0.5rem] border-8 z-10 border-light border-t-highlight border-l-highlight" />
+          <div className="rounded-full w-36 h-36 relative">
+            <div className="rounded-full absolute inset-[-0.5rem] bg-conic-gradient" />
+            <div className="rounded-full absolute inset-[0.3rem] bg-slate-900" />
             <span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-extrabold text-highlight text-4xl">
               58%
             </span>
