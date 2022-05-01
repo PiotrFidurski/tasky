d6c284ecb1822da5d5ca2bfeb9dceda478b6cded
pfidurski <crunor55@hotmail.com>
2022-05-01 12:24:08 +0200

    chore: load stats data for widget

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,6 +1,26 @@
+import { groupTasksByScheduledFor } from '~/models/task';
+import { getAuthUserId } from '~/session/session.server';
+
+import { LoaderFunction, useLoaderData } from 'remix';
+
 import { CompletedTasks } from '~/components/Widgets/CompletedTasks/';
 
+import { getDayStats } from '~/utils/getDayStats';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getAuthUserId(request);
+
+  const groupedTasks = await groupTasksByScheduledFor(userId);
+
+  const stats = getDayStats(groupedTasks);
+
+  return stats;
+};
+
 export default function HomeIndexRoute() {
+  const data = useLoaderData<{ [key: string]: number[] }>();
+
+  console.log({ data });
   return (
     <div>
       <CompletedTasks />
