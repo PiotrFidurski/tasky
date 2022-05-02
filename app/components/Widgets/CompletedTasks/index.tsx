60cae021eb1082aa264197076031234c7aaf69c3
pfidurski <crunor55@hotmail.com>
2022-05-02 09:16:11 +0200

    refactor: render total stats in a widget

--- app/components/Widgets/CompletedTasks/index.tsx
+++ app/components/Widgets/CompletedTasks/index.tsx
@@ -1,6 +1,12 @@
 import { CircularProgress } from './ProgressCircle';
 
-export function CompletedTasks() {
+type CompletedTasksProps = {
+  total: number;
+  completed: number;
+};
+
+export function CompletedTasks({ completed, total }: CompletedTasksProps) {
+  const percentage = (completed / total) * 100;
   return (
     <div className="w-full border-2 border-highlight rounded-2xl p-8 dark:bg-slate-900 bg-light text-darkGray dark:text-lightGray">
       <div className="flex items-center justify-between">
@@ -8,10 +14,16 @@ export function CompletedTasks() {
           <p className="font-extrabold text-xl mb-2">
             Great, your progress is almost done!
           </p>
-          <span className="font-semibold text-sm">4 of 7 completed</span>
+          <span className="font-semibold text-sm">
+            {completed} out of {total} completed
+          </span>
         </div>
         <div className="w-full flex justify-end">
-          <CircularProgress strokeWidth={10} squareSize={125} percentage={25} />
+          <CircularProgress
+            strokeWidth={10}
+            squareSize={125}
+            percentage={Number(percentage.toFixed(0))}
+          />
         </div>
       </div>
     </div>
