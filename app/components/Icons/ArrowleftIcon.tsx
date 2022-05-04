da146054acda9c5996b3a972c61b8a4f13621539
PiotrFidurski <crunor55@hotmail.com>
2022-05-04 17:19:48 +0200

    refactor: update Arrow icons

--- app/components/Icons/ArrowleftIcon.tsx
+++ app/components/Icons/ArrowleftIcon.tsx
@@ -6,12 +6,12 @@ export function ArrowleftIcon() {
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
+      strokeWidth={2}
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={2}
-        d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+        d="M10 19l-7-7m0 0l7-7m-7 7h18"
       />
     </svg>
   );
