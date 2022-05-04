59204e68dc5d588ea673bd45f83359b3f39d20c2
PiotrFidurski <crunor55@hotmail.com>
2022-05-04 16:41:04 +0200

    refactor: load stats for each day

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,9 +1,28 @@
+import { groupTasksByScheduledFor } from '~/models/task';
+import { getAuthUserId } from '~/session/session.server';
+
+import { LoaderFunction, useLoaderData } from 'remix';
+
 import { Calendar } from '~/components/Widgets/Calendar';
 
+import { getDayStats } from '~/utils/getDayStats';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getAuthUserId(request);
+
+  const groupedTasks = await groupTasksByScheduledFor(userId);
+
+  const stats = getDayStats(groupedTasks);
+
+  return stats;
+};
+
 export default function IndexRoute() {
+  const stats = useLoaderData<{ [key: string]: number[] }>();
+
   return (
     <div>
-      <Calendar date={new Date()} />
+      <Calendar date={new Date()} stats={stats} />
     </div>
   );
 }
