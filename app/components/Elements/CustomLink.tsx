fc6018607ae793739547a0ccb783e9f1e8b88d07
PiotrFidurski <crunor55@hotmail.com>
2022-05-06 19:04:40 +0200

    feat: add visual feedback to selected days

--- app/components/Elements/CustomLink.tsx
+++ app/components/Elements/CustomLink.tsx
@@ -5,12 +5,14 @@ import { Link } from 'remix';
 
 type CustomLinkProps = RemixLinkProps & {
   to: string;
+  isActive?: boolean;
 };
 
 export function CustomLink({
   to,
   children,
   className,
+  isActive = false,
   ...remixLinkProps
 }: CustomLinkProps) {
   return (
@@ -20,6 +22,7 @@ export function CustomLink({
         `flex justify-center hover:bg-active dark:hover:bg-active_dark
          focus:bg-active dark:focus:bg-active_dark focus:ring-highlight
          focus:ring-2 focus:ring-inset focus:outline-none transition-all`,
+        isActive && 'ring-2 ring-pink-500  bg-active_dark',
         className
       )}
       {...remixLinkProps}
--- app/components/Widgets/Calendar/Day.tsx
+++ app/components/Widgets/Calendar/Day.tsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx';
 
+import { useParams } from 'remix';
+
 import { CustomLink } from '~/components/Elements/CustomLink';
 
 import { getCalendarDayHelpers } from '~/utils/date';
@@ -14,16 +16,22 @@ export function Day({ day, date, stats }: DayProps) {
   const { isTodaysDate, dayOfMonth, isDateBeforeToday, isInThisMonth } =
     getCalendarDayHelpers(day, date);
 
+  const params = useParams();
+
   const [total, complete] = stats[day] ?? [];
+
+  const isActive = day === params.day;
 
   return (
     <CustomLink
+      isActive={isActive}
       to={`/home/${day}/completed`}
       className={clsx(
         'w-10 h-10 flex items-center justify-center rounded-full mb-2 text-s font-bold relative',
         isTodaysDate && 'border border-gray-700 text-highlight',
         isDateBeforeToday && !isTodaysDate && 'text-gray-500',
-        !isInThisMonth && 'text-gray-800'
+        !isInThisMonth && 'text-gray-800',
+        isActive && ''
       )}
     >
       {dayOfMonth}
