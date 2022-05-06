f13c9dfd79b19464f9f34f01acc905d57586f869
PiotrFidurski <crunor55@hotmail.com>
2022-05-06 19:07:52 +0200

    refactor: remove isActive from className

--- app/components/Widgets/Calendar/Day.tsx
+++ app/components/Widgets/Calendar/Day.tsx
@@ -30,8 +30,7 @@ export function Day({ day, date, stats }: DayProps) {
         'w-10 h-10 flex items-center justify-center rounded-full mb-2 text-s font-bold relative',
         isTodaysDate && 'border border-gray-700 text-highlight',
         isDateBeforeToday && !isTodaysDate && 'text-gray-500',
-        !isInThisMonth && 'text-gray-800',
-        isActive && ''
+        !isInThisMonth && 'text-gray-800'
       )}
     >
       {dayOfMonth}
