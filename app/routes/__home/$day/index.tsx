7f0b02c52fbbce95adcae943adf2d75806adf15c
PiotrFidurski <crunor55@hotmail.com>
2022-05-09 20:26:18 +0200

    refactor: display tasks for $day route in home dir

--- app/routes/__home/$day/index.tsx
+++ app/routes/__home/$day/index.tsx
@@ -1,7 +1,31 @@
-import { useParams } from 'remix';
+import { Task } from '@prisma/client';
+import { getTasksForDay } from '~/models/task';
+import { getAuthUserId } from '~/session/session.server';
+
+import { LoaderFunction, useLoaderData, useParams } from 'remix';
+
+import { TaskComponent } from '~/components/Tasks/TaskComponent';
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const userId = await getAuthUserId(request);
+  if (!params.day) return null;
+  const tasks = getTasksForDay(params.day, userId);
+
+  return tasks;
+};
 
 export default function IndexRoute() {
+  const loaderData = useLoaderData<Task[]>();
   const params = useParams<'day'>();
 
-  return <div>tasks for day: {params.day}</div>;
+  return (
+    <div>
+      <h1>tasks for day: {params.day}</h1>
+      <div>
+        {loaderData.map((task) => (
+          <TaskComponent task={task} key={task.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
