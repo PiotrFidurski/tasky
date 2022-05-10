ca4bc0d1e94908cbc8bb108afb97bc4ce1360b19
PiotrFidurski <crunor55@hotmail.com>
2022-05-10 18:34:32 +0200

    refactor: remove trailing console logs

--- app/components/Widgets/Calendar/Header.tsx
+++ app/components/Widgets/Calendar/Header.tsx
@@ -16,7 +16,7 @@ type HeaderProps = {
 
 export function Header({ date, onPrevMonth, onNextMonth }: HeaderProps) {
   const headerContent = getCalendarHeader(date);
-  console.log(headerContent);
+
   return (
     <div className="flex items-center justify-between w-full">
       <Button onClick={onPrevMonth} isIconWrapper className="w-auto">
