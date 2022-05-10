4dc63fd46bf72ebfac8e730b04e0005c15246c4f
PiotrFidurski <crunor55@hotmail.com>
2022-05-10 19:33:31 +0200

    refactor: add ErrorBoundary to home index route

--- app/routes/__home/$day.tsx
+++ app/routes/__home/$day.tsx
@@ -1,10 +1,19 @@
 import { groupTasksByScheduledFor } from '~/models/task';
 import { requireUserId } from '~/session/auth.server';
 
-import { LoaderFunction, Outlet, useLoaderData } from 'remix';
+import {
+  LoaderFunction,
+  Outlet,
+  useCatch,
+  useLoaderData,
+  useNavigate,
+} from 'remix';
 
+import { Button } from '~/components/Elements/Button';
+import { ArrowleftIcon } from '~/components/Icons/ArrowleftIcon';
 import { Calendar } from '~/components/Widgets/Calendar';
 
+import { formatDate } from '~/utils/date';
 import { getDayStats } from '~/utils/getDayStats';
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -24,6 +33,33 @@ export default function IndexRoute() {
     <div>
       <Calendar startingDate={new Date()} stats={stats} />
       <Outlet />
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="dark:text-custom__ghostly text-custom__gray mt-12 p-4 bg-red-400 rounded-md">
+      <div className="flex items-center mb-4">
+        <Button
+          isIconWrapper
+          className="w-auto"
+          onClick={() => navigate(`/${formatDate(new Date())}`)}
+        >
+          <ArrowleftIcon />
+        </Button>
+        <div className="w-full grid place-content-center text-center">
+          <p>{caught.status}</p>
+          <p>{caught.statusText}</p>
+        </div>
+      </div>
+      <details>
+        <code className="break-words whitespace-normal">{caught.data}</code>
+      </details>
     </div>
   );
 }
