d50d2b8355dd2e31ca4fb6c25d09486af51c5d0f
PiotrFidurski <crunor55@hotmail.com>
2022-05-11 20:04:59 +0200

    refactor: remove zIndex from animationv variants

--- app/components/Widgets/Calendar/animationVariants.ts
+++ app/components/Widgets/Calendar/animationVariants.ts
@@ -6,13 +6,11 @@ export const variants = {
     };
   },
   center: {
-    zIndex: 1,
     x: 0,
     opacity: 1,
   },
   exit: (direction: string) => {
     return {
-      zIndex: 0,
       x: direction === 'left' ? 300 : -300,
       opacity: 0,
     };
