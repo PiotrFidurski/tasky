431b8646de96f717798266879fa158285fdc158a
PiotrFidurski <crunor55@hotmail.com>
2022-05-11 20:45:51 +0200

    chore: add docs to constants

--- app/components/Widgets/Calendar/consts.ts
+++ app/components/Widgets/Calendar/consts.ts
@@ -1,4 +1,10 @@
-const HEADER_SIZE = 80;
-const POSTION_TOP = 64;
+/**
+ * Sizes for calendar to calculate min height.
+ *
+ * @remarks numbers represent `rem` values eg 4rem --> 64px
+ */
+const ROW_SIZE = 3;
+const HEADER_SIZE = 5;
+const POSTION_TOP = 4;
 
-export { HEADER_SIZE, POSTION_TOP };
+export { HEADER_SIZE, POSTION_TOP, ROW_SIZE };
