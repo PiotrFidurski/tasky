82e081f3854eeed50ac0b4fdfd30dbf68115d670
PiotrFidurski <crunor55@hotmail.com>
2022-05-12 20:13:48 +0200

    refactor: redirect to todays date in index route

--- app/routes/__home/index.tsx
+++ app/routes/__home/index.tsx
@@ -1,3 +1,11 @@
+import { LoaderFunction, redirect } from 'remix';
+
+import { formatDate } from '~/utils/date';
+
+export const loader: LoaderFunction = async () => {
+  return redirect(`/${formatDate()}`);
+};
+
 export default function IndexRoute() {
-  return <div>index route</div>;
+  return null;
 }
