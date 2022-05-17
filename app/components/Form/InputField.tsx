186267aeb5c51a8c08f76a0b93084c3c25b3ad8c
PiotrFidurski <crunor55@hotmail.com>
2022-05-17 19:45:39 +0200

    feat: update input field styling

--- app/components/Form/InputField.tsx
+++ app/components/Form/InputField.tsx
@@ -8,7 +8,7 @@ export const InputField = forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         ref={ref}
-        className="border-b-2 mt-2 p-2 w-full focus:outline-none focus:border-b-indigo-600 focus:bg-indigo-100 transition-colors"
+        className="border-b-2 mt-2 p-2 w-full focus:outline-none focus:border-b-highlight focus:bg-indigo-100 transition-colors"
         {...htmlInputProps}
       />
     );
