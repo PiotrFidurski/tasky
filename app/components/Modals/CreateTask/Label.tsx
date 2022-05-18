0a882f0528c409b2a65e5a1ade2078bd7797965f
PiotrFidurski <crunor55@hotmail.com>
2022-05-18 19:15:49 +0200

    feat: add styling to Label

--- app/components/Modals/CreateTask/Label.tsx
+++ app/components/Modals/CreateTask/Label.tsx
@@ -9,8 +9,12 @@ export function Label({
   ...htmlLabelProps
 }: FieldWrapperProps) {
   return (
-    <label htmlFor={htmlFor} {...htmlLabelProps}>
-      <span>{htmlFor}</span>
+    <label
+      htmlFor={htmlFor}
+      {...htmlLabelProps}
+      className="p-2 border border-gray-500 rounded-md flex flex-col items-center justify-center"
+    >
+      <span className="sr-only">{htmlFor}</span>
       {children}
       <div>
         <span
