90f29fad72c3afa781d0f61f6f763ec1fcab6665
PiotrFidurski <crunor55@hotmail.com>
2022-05-18 18:38:08 +0200

    refactor: extract Form component

---
+++ app/components/Modals/CreateTask/FormComponent.tsx
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+import { ZodTaskErrors } from '~/validation/task';
+
+import { Form, useActionData } from 'remix';
+
+import { Button } from '~/components/Elements/Button';
+import { FieldWrapper } from '~/components/Form/FieldWrapper';
+import { InputField } from '~/components/Form/InputField';
+
+import { useErrors } from '~/utils/hooks/useErrors';
+
+type ActionData = z.infer<typeof ZodTaskErrors>;
+
+export function FormComponent() {
+  const actionData = useActionData<ActionData>();
+
+  const { fieldErrors } = useErrors(actionData);
+
+  return (
+    <div className="p-4 text-custom__gray">
+      <Form method="post" className="p-4">
+        <div className="w-full mb-2">
+          <FieldWrapper htmlFor="task" errorMessage={fieldErrors?.body || ''}>
+            <InputField
+              placeholder="What do you want to do today?"
+              required
+              aria-label="body"
+              name="body"
+              id="task"
+            />
+          </FieldWrapper>
+        </div>
+        <div className="flex justify-end">
+          <Button
+            className="rounded-full font-bold px-4 py-2 justify-center"
+            primary
+          >
+            <span>Add Task</span>
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
--- app/components/Modals/CreateTask/index.tsx
+++ app/components/Modals/CreateTask/index.tsx
@@ -1,25 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { useState } from 'react';
-import { z } from 'zod';
-import { ZodTaskErrors } from '~/validation/task';
 
-import { Form, useActionData, useNavigate, useParams } from 'remix';
+import { useNavigate, useParams } from 'remix';
 
-import { Button } from '~/components/Elements/Button';
-import { FieldWrapper } from '~/components/Form/FieldWrapper';
-import { InputField } from '~/components/Form/InputField';
-
-import { useErrors } from '~/utils/hooks/useErrors';
-
+import { FormComponent } from './FormComponent';
 import { Header } from './Header';
 
-type ActionData = z.infer<typeof ZodTaskErrors>;
-
 export function CreateTask() {
-  const actionData = useActionData<ActionData>();
-
-  const { fieldErrors } = useErrors(actionData);
-
   const navigate = useNavigate();
 
   const { day } = useParams<'day'>();
@@ -39,32 +26,7 @@ export function CreateTask() {
         <Dialog.Overlay className="bg-custom__gray dark:bg-custom__ghostly fixed inset-0 opacity-20 z-50" />
         <Dialog.Content className="z-50 absolute inset-0 h-full bottom-auto dark:bg-slate-900 bg-light text-darkGray dark:text-lightGray max-w-full lg:max-w-lg m-auto">
           <Header>Create task</Header>
-          <div className="p-4 text-custom__gray">
-            <Form method="post" className="p-4">
-              <div className="w-full mb-2">
-                <FieldWrapper
-                  htmlFor="task"
-                  errorMessage={fieldErrors?.body || ''}
-                >
-                  <InputField
-                    placeholder="What do you want to do today?"
-                    required
-                    aria-label="body"
-                    name="body"
-                    id="task"
-                  />
-                </FieldWrapper>
-              </div>
-              <div className="flex justify-end">
-                <Button
-                  className="rounded-full font-bold px-4 py-2 justify-center"
-                  primary
-                >
-                  <span>Add Task</span>
-                </Button>
-              </div>
-            </Form>
-          </div>
+          <FormComponent />
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
