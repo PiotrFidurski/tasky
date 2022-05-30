cc99516a24a9ebe9eff4eb577f618d755062c905
PiotrFidurski <crunor55@hotmail.com>
2022-05-30 18:51:27 +0200

    refactor: extract modal classNames
    move content and overlay classNames to own file

---
+++ app/components/Modals/classNames.ts
@@ -0,0 +1,5 @@
+export const modalOverlay =
+  'bg-custom__gray dark:bg-custom__ghostly fixed inset-0 opacity-20 z-50';
+
+export const modalContent =
+  'z-50 absolute inset-0 h-full bottom-auto dark:bg-slate-900 bg-light text-darkGray dark:text-lightGray max-w-full lg:max-w-lg m-auto';
