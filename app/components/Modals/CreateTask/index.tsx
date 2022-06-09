0e7873ba2d49c72ad793e8112fc6f071d1743748
PiotrFidurski <crunor55@hotmail.com>
2022-06-09 19:24:06 +0200

    feat: clear draft data on modal close

--- app/components/Modals/CreateTask/index.tsx
+++ app/components/Modals/CreateTask/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { useState } from 'react';
 
-import { Form, useNavigate } from 'remix';
+import { Form, useFetcher, useNavigate } from 'remix';
 
 import { modalContent, modalOverlay } from '../classNames';
 import { Header } from '../components/Header';
@@ -13,27 +13,31 @@ type CreateTaskProps = {
 
 export function CreateTask({ draft }: CreateTaskProps) {
   const navigate = useNavigate();
-
+  const fetcher = useFetcher();
   const [open, setOpen] = useState(true);
 
   const handleOpenChange = () => {
-    // fetcher.submit({ reset: 'reset' }, { method: 'post' });
-    // setOpen(false);
-    // navigate(-1);
+    fetcher.submit({ reset: 'reset' }, { method: 'post' });
+
+    setOpen(false);
+
+    navigate(-1);
   };
 
   return (
-    <Dialog.Root open={open} onOpenChange={handleOpenChange}>
-      <Dialog.Trigger />
-      <Dialog.Portal>
-        <Dialog.Overlay className={modalOverlay} />
-        <Form method="post">
-          <Dialog.Content className={modalContent}>
-            <Header srDescription="Create task dialog">Create task</Header>
-            <FormComponent draft={draft} />
-          </Dialog.Content>
-        </Form>
-      </Dialog.Portal>
-    </Dialog.Root>
+    <Form method="post">
+      <Dialog.Root open={open} onOpenChange={handleOpenChange}>
+        <Dialog.Trigger />
+        <Dialog.Portal>
+          <Form method="post">
+            <Dialog.Overlay className={modalOverlay} />
+            <Dialog.Content className={modalContent}>
+              <Header srDescription="Create task dialog">Create task</Header>
+              <FormComponent draft={draft} />
+            </Dialog.Content>
+          </Form>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </Form>
   );
 }
