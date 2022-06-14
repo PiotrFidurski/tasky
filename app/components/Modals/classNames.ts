7837c54b3f6ddf20e6b394040f819d9f248cf2eb
PiotrFidurski <crunor55@hotmail.com>
2022-06-14 18:24:22 +0200

    feat: update modal sizes, adjust overlays

--- app/components/Modals/classNames.ts
+++ app/components/Modals/classNames.ts
@@ -1,4 +1,4 @@
-export const modalOverlay = 'fixed inset-0 opacity-20 z-50';
+export const modalOverlay = 'fixed inset-0 opacity-50 z-50 bg-gray-500';
 
 export const modalContent =
-  'z-50 absolute inset-0 h-full bottom-auto dark:bg-slate-900 bg-light max-w-full lg:max-w-lg m-auto flex items-center flex-col';
+  'z-50 absolute inset-0 md:top-12 md:rounded-md md:h-auto h-full bottom-auto dark:bg-slate-900 bg-light max-w-full lg:max-w-lg m-auto flex items-center flex-col';
