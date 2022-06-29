4c635672a044d44d7ebfcda61a0228770a2672b7
PiotrFidurski <crunor55@hotmail.com>
2022-06-29 20:06:54 +0200

    refactor: submit an action with fetcher on close

--- app/components/Modals/CreateTask/index.tsx
+++ app/components/Modals/CreateTask/index.tsx
@@ -1,5 +1,8 @@
 import * as Dialog from '@radix-ui/react-dialog';
 
+import { useFetcher, useNavigate, useSearchParams } from 'remix';
+
+import { DESTROY_DRAFT } from '../actionTypes';
 import { modalContent, modalOverlay } from '../classNames';
 import { Header } from '../components/Header';
 import { FormComponent } from './FormComponent';
@@ -9,8 +12,24 @@ type CreateTaskProps = {
 };
 
 export function CreateTask({ draft }: CreateTaskProps) {
+  const navigate = useNavigate();
+
+  const fetcher = useFetcher();
+
+  const [searchParams] = useSearchParams();
+
+  const handleOpenChange = () => {
+    fetcher.submit({ _action: DESTROY_DRAFT }, { method: 'post' });
+
+    if (searchParams.get('selectedDate')) {
+      return navigate(-2);
+    }
+
+    return navigate(-1);
+  };
+
   return (
-    <Dialog.Root defaultOpen>
+    <Dialog.Root defaultOpen onOpenChange={handleOpenChange}>
       <Dialog.Trigger />
       <Dialog.Portal>
         <Dialog.Overlay className={modalOverlay} />
