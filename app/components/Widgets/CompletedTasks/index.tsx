b077ba8d58b8bd076addb0e050752c2fab0ea77c
PiotrFidurski <crunor55@hotmail.com>
2022-07-19 19:44:26 +0200

    refactor: use shadow tw color

--- app/components/Widgets/CompletedTasks/index.tsx
+++ app/components/Widgets/CompletedTasks/index.tsx
@@ -16,10 +16,7 @@ export function CompletedTasks({
 }: CompletedTasksProps) {
   return (
     <div className="w-full max-w-sm rounded-2xl dark:bg-slate-900 bg-light z-10">
-      <div
-        style={{ boxShadow: '0px 1px 0px 0px #20283c' }}
-        className="flex items-center justify-between w-full p-8"
-      >
+      <div className="flex items-center justify-between w-full p-8 shadow-[0px_1px_0px_0px] shadow-shadowHighlight">
         <TextContent total={total} completed={completed} />
         <div className="w-full flex justify-end">
           <CircularProgress
