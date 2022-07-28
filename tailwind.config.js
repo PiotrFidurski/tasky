0f298586d9fa91c011e51498425c1b3c4fe3d848
PiotrFidurski <crunor55@hotmail.com>
2022-07-28 20:09:35 +0200

    config: update tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,13 @@ module.exports = {
         shadowHighlight: '#26327636',
         // LightMode
         primary: '#F4F6FD',
-        secondary: '#0a1638',
+        secondary: '#0A1638',
         // #0a1244 0A155A
         borderAndIcons: '#ADBAEB',
         blacker: '#020417',
-        textGray: '#9d9ab4',
-        darkgray: '#373b5e',
-        buttonColor: '#2643c4',
+        textGray: '#9D9AB4',
+        darkgray: '#373B5E',
+        buttonColor: '#2643C4',
       },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif', ...defaultTheme.fontFamily.sans],
