33d49b71486a4bc9990f388d255c6d92b70e7fba
PiotrFidurski <crunor55@hotmail.com>
2022-08-01 20:00:46 +0200

    refactor: remove template literal

--- app/components/Elements/NavListItemv2.tsx
+++ app/components/Elements/NavListItemv2.tsx
@@ -10,10 +10,7 @@ export function NavListItem({ children, to, ...htmlLiProps }: ListItemProps) {
     <li {...htmlLiProps} className="text-secondary dark:text-primary">
       <NavLink
         to={to}
-        className={`py-3 outline-none flex w-full font-normal  px-2 
-        focus:text-buttonColor dark:focus:text-borderAndIcons underline-offset-2 focus:underline
-         hover:text-buttonColor dark:hover:text-borderAndIcons hover:underline
-        `}
+        className="py-3 outline-none flex w-full font-normal px-2 focus:text-buttonColor dark:focus:text-borderAndIcons underline-offset-2 focus:underline hover:text-buttonColor dark:hover:text-borderAndIcons hover:underline"
       >
         {({ isActive }) => (
           <div className="flex items-center gap-4">
