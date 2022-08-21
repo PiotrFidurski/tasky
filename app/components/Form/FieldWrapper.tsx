4f19b1031babc2acaeecdedef644d7524df2e7e2
PiotrFidurski <crunor55@hotmail.com>
2022-08-21 10:44:41 +0200

    refactor: update form components styles

--- app/components/Form/FieldWrapper.tsx
+++ app/components/Form/FieldWrapper.tsx
@@ -22,17 +22,17 @@ export function FieldWrapper({
     <label
       htmlFor={htmlFor}
       {...htmlLabelProps}
-      className={clsx(
-        'mb-4 w-full border-2 dark:border-white border-black dark:text-white rounded-md flex relative min-h-[4.5rem] dark:focus-within:border-highlight focus-within:border-highlight dark:focus-within:text-highlight focus-within:text-highlight transition-colors',
-        className
-      )}
+      className={`mb-4 w-full border-2 dark:border-primary border-secondary
+       dark:text-primary rounded-md flex relative min-h-[5.5rem] text-secondary
+       dark:focus-within:border-custom-indigo focus-within:border-custom-blue
+       dark:focus-within:text-custom-indigo focus-within:text-custom-blue transition-colors`}
     >
       <div className="flex justify-between items-center w-full">
         <div className="w-full items-baseline flex flex-col h-full justify-between">
           <motion.div
             className={clsx(
-              'px-2 text-sm font-normal min-w-[16rem] text-left',
-              !errorMessage && 'mt-1'
+              'px-2 text-xs font-normal min-w-[16rem] text-left',
+              !errorMessage ? 'mt-1' : 'bg-primary dark:bg-secondary'
             )}
             layout
             initial={{ opacity: 0 }}
--- app/components/Form/InputField.tsx
+++ app/components/Form/InputField.tsx
@@ -8,7 +8,7 @@ export const InputField = forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         ref={ref}
-        className="text-black dark:text-white bg-transparent w-full h-full py-2 px-2 rounded-br-[0.200rem] rounded-bl-[0.200rem]"
+        className="text-secondary dark:text-primary bg-transparent w-full h-full py-2 px-2 rounded-br-[0.200rem] rounded-bl-[0.200rem]"
         {...htmlInputProps}
       />
     );
