7c51485a4c180cfd0a9bcaf599270f4a7f55a67a
PiotrFidurski <crunor55@hotmail.com>
2022-08-23 17:50:49 +0200

    refactor: PER-35 remove z-index from widget

--- app/components/Widgets/CompletedTasks/index.tsx
+++ app/components/Widgets/CompletedTasks/index.tsx
@@ -13,7 +13,7 @@ export function CompletedTasks({
   percentage,
 }: CompletedTasksProps) {
   return (
-    <div className="w-full max-w-sm rounded-2xl z-10 bg-light-rgba dark:bg-dark-rgba">
+    <div className="w-full max-w-sm mb-4 rounded-2xl bg-light-rgba dark:bg-dark-rgba">
       <div className="flex items-center justify-between w-full p-8">
         <TextContent total={total} completed={completed} />
         <div className="w-full flex justify-end">
