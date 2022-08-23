75ed42b20d4c5b886cc8ad75c0b6a32023edabf5
PiotrFidurski <crunor55@hotmail.com>
2022-08-23 17:11:55 +0200

    refactor: PER-35 remove unused css

--- app/components/Sidebar/mobile.tsx
+++ app/components/Sidebar/mobile.tsx
@@ -2,11 +2,12 @@ import { twMerge } from 'tailwind-merge';
 
 import { useState } from 'react';
 
+import { Button } from '../Elements/Button';
+import { HamburgerIcon } from '../Icons/HamburgerIcon';
 import { Menu } from './Menu';
-import { OpenMenuButton } from './OpenMenuButton';
 
 export function MobileSidebar() {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(true);
 
   const handleOpenMenu = () => {
     setExpanded(true);
@@ -17,15 +18,21 @@ export function MobileSidebar() {
   };
 
   return (
-    <div className="relative w-full md:hidden">
+    <div className="md:hidden">
       <div className="p-2">
-        <OpenMenuButton show={expanded} onHandleOpen={handleOpenMenu} />
+        <Button
+          className="border-transparent dark:border-transparent"
+          onClick={handleOpenMenu}
+          aria-controls="sidebar"
+          aria-label="open sidebar"
+          aria-expanded={expanded}
+        >
+          <HamburgerIcon />
+        </Button>
       </div>
       <div
         className={twMerge(
-          `font-semibold text-lg flex flex-col justify-between fixed top-0 
-          left-0 bottom-0 max-w-full z-50 bg-primary dark:bg-secondary w-full
-          h-full py-4 transition-all`,
+          `text-lg flex flex-col fixed inset-0 max-w-full z-50 bg-primary dark:bg-secondary transition-all`,
           expanded ? 'translate-x-0 visible' : 'translate-x-[-100%] invisible'
         )}
       >
