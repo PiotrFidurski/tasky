0ec22faf04208227f2c731b63a58108af42ca192
PiotrFidurski <crunor55@hotmail.com>
2022-08-30 17:35:58 +0200

    refactor: PER-38 update $day route

--- app/routes/__home/$day.tsx
+++ app/routes/__home/$day.tsx
@@ -1,12 +1,20 @@
 import nProgressStyles from 'nprogress/nprogress.css';
 
-import { format } from 'date-fns';
+import { z } from 'zod';
+
+import { format, isValid } from 'date-fns';
 
 import { LinksFunction, LoaderArgs, json } from 'remix';
 
-import { Outlet, useCatch, useLoaderData, useNavigate } from '@remix-run/react';
+import {
+  Outlet,
+  useCatch,
+  useLoaderData,
+  useNavigate,
+  useParams,
+} from '@remix-run/react';
 
-import { groupTasksByScheduledFor } from '~/server/models/task';
+import { getTasksForDay, groupTasksByScheduledFor } from '~/server/models/task';
 import { requireUserId } from '~/server/session/auth.server';
 
 import { Button } from '~/components/Elements/Button';
@@ -15,6 +23,7 @@ import { Calendar } from '~/components/Widgets/Calendar';
 import { DayLink } from '~/components/Widgets/Calendar/components/DayLink';
 import { CompletedTasks } from '~/components/Widgets/CompletedTasks';
 
+import { badRequest } from '~/utils/badRequest';
 import { DATE_FORMAT } from '~/utils/date';
 import { useRouteTransition } from '~/utils/hooks/useRouteTransition';
 import { getTaskStatsForEachDay, getTotalTasksCount } from '~/utils/taskStats';
@@ -23,8 +32,22 @@ export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: nProgressStyles }];
 };
 
-export async function loader({ request }: LoaderArgs) {
+export async function loader({ request, params }: LoaderArgs) {
   const userId = await requireUserId(request);
+
+  const day = z
+
+    .string({ invalid_type_error: 'expected a string.' })
+    .parse(params.day);
+
+  if (!isValid(new Date(day))) {
+    throw badRequest(
+      'No tasks found for this date, please check if the date is a valid date format (yyyy-MM-dd) eg: "2022-02-22".',
+      404
+    );
+  }
+
+  const tasks = await getTasksForDay(day, userId);
 
   const groupedTasks = await groupTasksByScheduledFor(userId);
 
@@ -39,13 +62,15 @@ export async function loader({ request }: LoaderArgs) {
     completed,
     total,
     percentage: !Number.isNaN(Number(percentage)) ? Number(percentage) : 0,
+    tasks,
   };
 
   return json(data, { status: 200 });
 }
 
 export default function DayRoute() {
-  const { completed, percentage, total, stats } =
+  const params = useParams<'day'>();
+  const { completed, percentage, total, stats, tasks } =
     useLoaderData<typeof loader>();
 
   useRouteTransition();
@@ -62,6 +87,16 @@ export default function DayRoute() {
         completed={completed}
         percentage={percentage}
       />
+      <div className="w-full max-w-sm bg-light-rgba dark:bg-dark-rgba">
+        <h1>tasks for day: {params.day}</h1>
+        <div>
+          {tasks.map((task) => (
+            <div key={task.id}>
+              <span>{task.body}</span>
+            </div>
+          ))}
+        </div>
+      </div>
       <Outlet />
     </>
   );
