f7a9434af5e7b760aa56e0ace5bbe3247f0009ac
PiotrFidurski <crunor55@hotmail.com>
2022-08-30 18:31:43 +0200

    feat: PER-38 increase spinner size
    prevents button height changing

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -2,7 +2,7 @@ export function Spinner() {
   return (
     <div
       className={`
-        w-5 h-5 border-2 bg-light-rgba dark:bg-dark-rgba border-custom-indigo  dark:border-custom-blue
+        w-6 h-6 border-2 bg-light-rgba dark:bg-dark-rgba border-custom-indigo  dark:border-custom-blue
       dark:border-b-dark-rgba dark:border-l-dark-rgba dark:border-r-dark-rgba
       border-b-dark-rgba border-l-dark-rgba border-r-dark-rgba animate-spin rounded-full
     `}
