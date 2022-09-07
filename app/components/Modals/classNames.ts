6898ebca0207f54044c1baacb0e976505612dde7
PiotrFidurski <crunor55@hotmail.com>
2022-09-07 18:22:39 +0200

    refactor: PER-46 avoid layout shift
    fixed an issue when user would click go back button and the create task
    modal would shift up

--- app/components/Modals/classNames.ts
+++ app/components/Modals/classNames.ts
@@ -1,4 +1,4 @@
 export const modalOverlay = 'fixed inset-0 opacity-50 z-50 bg-gray-500';
 
 export const modalContent =
-  'z-50 h-screen absolute inset-0 md:top-12 md:rounded-md md:h-auto md:pb-8 bottom-auto dark:bg-secondary bg-primary max-w-full lg:max-w-lg m-auto flex items-center flex-col';
+  'z-50 h-screen fixed inset-0 md:top-12 md:rounded-md md:h-auto md:pb-8 bottom-auto dark:bg-secondary bg-primary max-w-full lg:max-w-lg m-auto flex items-center flex-col';
