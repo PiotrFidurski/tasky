2baf29d3c8804b756655e1d25610cf6ca7b967c7
PiotrFidurski <crunor55@hotmail.com>
2022-09-16 19:27:26 +0200

    refactor: PER-18 extract DropdownContent component

---
+++ app/components/Elements/DropdownContent.tsx
@@ -0,0 +1,26 @@
+import {
+  Content,
+  DropdownMenuContentProps,
+} from '@radix-ui/react-dropdown-menu';
+import { twMerge } from 'tailwind-merge';
+
+import { forwardRef } from 'react';
+
+type DropdownContentProps = DropdownMenuContentProps &
+  React.RefAttributes<HTMLDivElement>;
+
+export const DropdownContent = forwardRef<HTMLDivElement, DropdownContentProps>(
+  ({ className, children, ...dropdownContentProps }, ref) => (
+    <Content
+      ref={ref}
+      className={twMerge(
+        `rounded-lg min-w-[14rem] dark:bg-secondary bg-primary 
+         shadow-custom-light dark:shadow-custom-dark`,
+        className
+      )}
+      {...dropdownContentProps}
+    >
+      {children}
+    </Content>
+  )
+);
