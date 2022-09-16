5a9f10b081723f18d2f4528c533b50a72ec74ff7
PiotrFidurski <crunor55@hotmail.com>
2022-09-16 19:27:01 +0200

    refactor: PER-18 add last and first item props

--- app/components/Elements/DropdownItem.tsx
+++ app/components/Elements/DropdownItem.tsx
@@ -3,17 +3,24 @@ import { twMerge } from 'tailwind-merge';
 
 import { forwardRef } from 'react';
 
-type DropdownItemProps = DropdownMenuItemProps &
-  React.RefAttributes<HTMLDivElement>;
+type DropdownItemProps = DropdownMenuItemProps & {
+  isLastItem?: boolean;
+  isFirstItem?: boolean;
+} & React.RefAttributes<HTMLDivElement>;
 
 export const DropdownItem = forwardRef<HTMLDivElement, DropdownItemProps>(
-  ({ className, children, ...dropdownItemProps }, ref) => (
+  (
+    { className, children, isLastItem, isFirstItem, ...dropdownItemProps },
+    ref
+  ) => (
     <Item
       ref={ref}
       className={twMerge(
         `text-secondary dark:text-primary 
          cursor-pointer px-2 py-4 flex items-center gap-4
          outline-none focus:bg-[rgba(0,0,0,0.03)] dark:focus:bg-[rgba(255,255,255,0.05)]`,
+        isFirstItem && 'rounded-tl-md rounded-tr-md',
+        isLastItem && 'rounded-bl-md rounded-br-md',
         className
       )}
       {...dropdownItemProps}
