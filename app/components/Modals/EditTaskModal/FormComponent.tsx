73192fea7a1cf92eed6ee8139a4799eb3515346c
PiotrFidurski <crunor55@hotmail.com>
2022-10-20 17:37:09 +0200

    refactor: PER-18 show spinner during submission

--- app/components/Modals/EditTaskModal/FormComponent.tsx
+++ app/components/Modals/EditTaskModal/FormComponent.tsx
@@ -3,19 +3,28 @@ import { useEffect, useRef } from 'react';
 import { Form, useLoaderData, useParams } from '@remix-run/react';
 
 import { actionTypes } from '~/server/actions/actionTypes';
+import { loader } from '~/server/loaders/taskId.edit.server';
 
 import { Button } from '~/components/Elements/Button';
 import { FieldWrapper } from '~/components/Form/FieldWrapper';
 import { InputField } from '~/components/Form/InputField';
+import { CaretUp } from '~/components/Icons/CaretUp';
+import { Spinner } from '~/components/Spinner';
 
+import { useActionTransition } from '~/utils/hooks/useActionTransition';
 import { useRouteData } from '~/utils/hooks/useRouteData';
 
 import { JsonifiedUser } from '~/types';
 
 export function FormComponent() {
-  const { body } = useLoaderData();
+  const { body, scheduledFor } = useLoaderData<typeof loader>();
+
+  const { isSubmitting, transition } = useActionTransition();
+
   const data = useRouteData<{ user: JsonifiedUser }>('root');
+
   const params = useParams<'day' | 'taskId'>();
+
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
@@ -28,6 +37,7 @@ export function FormComponent() {
     <Form method="post" className="w-full p-4" action={`/${params.day}`}>
       <input type="hidden" name="id" value={params.taskId} />
       <input type="hidden" name="ownerId" value={data?.user?.id} />
+      <input type="hidden" name="scheduledFor" value={scheduledFor} />
       <FieldWrapper htmlFor="body" errorMessage="" labelName="Task body">
         <InputField ref={inputRef} defaultValue={body} name="body" />
       </FieldWrapper>
@@ -37,8 +47,16 @@ export function FormComponent() {
           type="submit"
           name="_action"
           value={actionTypes.UPDATE_TASK}
+          className="flex items-center gap-4 px-8 py-4 text-sm shadow-md shadow-shadowSecondary dark:shadow-shadowPrimary"
         >
-          Update
+          <span>Update</span>
+          {isSubmitting &&
+          transition.submission?.formData.get('_action') ===
+            actionTypes.UPDATE_TASK ? (
+            <Spinner />
+          ) : (
+            <CaretUp />
+          )}
         </Button>
       </div>
     </Form>
