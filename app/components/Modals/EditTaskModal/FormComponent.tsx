89f396dae2e4b318910f37b50ab261fe9508fbf9
PiotrFidurski <crunor55@hotmail.com>
2022-10-27 19:01:39 +0200

    refactor: PER-18 add margin bottom to task input

--- app/components/Modals/EditTaskModal/FormComponent.tsx
+++ app/components/Modals/EditTaskModal/FormComponent.tsx
@@ -56,6 +56,7 @@ export function FormComponent() {
         htmlFor="body"
         errorMessage={fieldErrors?.body || ''}
         labelName="Task body"
+        className="mb-3"
       >
         <InputField ref={inputRef} defaultValue={body} name="body" id="body" />
       </FieldWrapper>
